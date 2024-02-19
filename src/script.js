18dd07b3b35d5a37d0528f6bdc868d0f23dea4b3
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-02-19 15:56:57 +0100

    fixed bug

--- src/script.js
+++ src/script.js
@@ -29,7 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault(); // Prevent the default form submission behavior
 
         try {
-<<<<<<< HEAD
 
 
             const date = new Date(dataDiConvocazione);
@@ -43,9 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(formattedDate1); // Esempio di output: "15/04/23"
 
             const response = await fetch('../assets/rai.docx');
-=======
-            const response = await fetch('./assets/rai.docx');
->>>>>>> e6664f48119e45b56544afacbabbdfe3af4fcee4
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
